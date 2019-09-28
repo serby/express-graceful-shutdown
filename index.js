ab38739da8cff0d2139bc0720d69ec8071e6faef
Paul Serby <paul.serby@clock.co.uk>
2019-09-28 22:13:27 +0100

    feat: Log as `info` on SIGTERM. It's normal but causes false alerts

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 function createMiddleware(server, opts) {
-
-  var shuttingDown = false
-    , options =
-      { logger: console
-      , forceTimeout: 30000, ...opts }
+  var shuttingDown = false,
+    options = { logger: console, forceTimeout: 30000, ...opts }
 
   // Graceful shutdown taken from: http://blog.argteam.com/
   process.on('SIGTERM', gracefulExit)
 
   function gracefulExit() {
-
     // Don't bother with graceful shutdown on development to speed up round trip
     if (!process.env.NODE_ENV) return process.exit(1)
 
     if (shuttingDown) return
     shuttingDown = true
-    options.logger.warn('Received kill signal (SIGTERM), shutting down')
+    options.logger.info('Received kill signal (SIGTERM), shutting down')
 
-    setTimeout(function () {
-      options.logger.error('Could not close connections in time, forcefully shutting down')
+    setTimeout(function() {
+      options.logger.error(
+        'Could not close connections in time, forcefully shutting down'
+      )
       process.exit(1)
     }, options.forceTimeout).unref()
 
-    server.close(function () {
+    server.close(function() {
       options.logger.info('Closed out remaining connections.')
       process.exit()
     })
-
   }
 
   function middleware(req, res, next) {
@@ -36,7 +33,6 @@ function createMiddleware(server, opts) {
   }
 
   return middleware
-
 }
 
 module.exports = createMiddleware
